0f5da9bc0fddb3f3affae8f1bff44ffeb0865cfe
AlanGL98 <71118102+AlanGL98@users.noreply.github.com>
2022-10-26 23:13:01 -0500

    dependencias de botones e iconos
    se agrega la dependencia de iconos y botones para diseño de las paginas

--- app_yopino/src/index.js
+++ app_yopino/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import WebFont from 'webfontloader';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
 
 WebFont.load({
   google: {
@@ -14,6 +17,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Button variant="contained" disableElevation>Astro</Button>
+    <Button variant="outlined" startIcon={<DeleteIcon />}>
+      Delete
+    </Button>
+    <Button variant="contained" endIcon={<SendIcon />}>
+      Send
+    </Button>
   </React.StrictMode>
 );
 
