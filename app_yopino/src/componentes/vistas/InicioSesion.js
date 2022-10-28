a37046c8444c84ed54fa8d0a4e2270b106fb164a
venegaspato <patricia.venegasvllrr@uanl.edu.mx>
2022-10-27 21:44:39 -0500

    LOGIN Y SIGNUP BIEN PRROS

--- app_yopino/src/componentes/vistas/InicioSesion.js
+++ app_yopino/src/componentes/vistas/InicioSesion.js
@@ -1,17 +1,72 @@
 import React from 'react';
-import {Helmet} from 'react-helmet';
-import HeaderDiv from '../HeaderDiv';
+import * as Components from '../../elementos/logsign';
+import { Link } from 'react-router-dom';
+// import {Helmet} from 'react-helmet';
+// import HeaderDiv from '../HeaderDiv';
 
-const InicioSesion = () => {
-    return (  
-        <>
-            <Helmet>
-                <title>Inicio de sesion</title>
-            </Helmet>
+
+// const InicioSesion = () => {
+//     return (  
+//         <>
+//             <Helmet>
+//                 <title>Inicio de sesion</title>
+//             </Helmet>
             
-            <HeaderDiv titulo="Iniciar sesion"/>
-        </>
-    );
+//             <HeaderDiv titulo="Iniciar sesion"/>
+
+//         </>
+
+        
+//     );
+// }
+
+function InicioSesion() {
+    const [signIn, toggle] = React.useState(true);
+     return(
+         <Components.Container>
+             <Components.SignUpContainer signinIn={signIn}>
+                 <Components.Form>
+                     <Components.Title>Crear cuenta</Components.Title>
+                     <Components.Input type='text' placeholder='Nombre' />
+                     <Components.Input type='text' placeholder='Apellidos' />
+                     <Components.Input type='text' placeholder='Nombre de usuario' />
+                     <Components.Input type='email' placeholder='Email' />
+                     <Components.Input type='password' placeholder='Password' />
+                     <Link to={"/"}><Components.Button>Registrarse</Components.Button></Link>
+                    
+                 </Components.Form>
+             </Components.SignUpContainer>
+
+             <Components.SignInContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Iniciar Sesion</Components.Title>
+                      <Components.Input type='text' placeholder='Usuario' />
+                      <Components.Input type='password' placeholder='Password' />
+                    
+                      <Link to={"/"}><Components.Button>Iniciar Sesion</Components.Button></Link>
+                  </Components.Form>
+             </Components.SignInContainer>
+
+             <Components.OverlayContainer signinIn={signIn}>
+                 <Components.Overlay signinIn={signIn}>
+
+                 <Components.LeftOverlayPanel signinIn={signIn}>
+                     <Components.Title>Bienvenido de vuelta</Components.Title>
+                     <Components.Paragraph> Inicia sesión para seguir dando tus opiniones</Components.Paragraph>
+                     <Components.GhostButton onClick={() => toggle(true)}> Iniciar Sesión </Components.GhostButton>
+                     </Components.LeftOverlayPanel>
+
+                     <Components.RightOverlayPanel signinIn={signIn}>
+                       <Components.Title>Bienvenido</Components.Title>
+                       <Components.Paragraph>Llena tu informacion y comienza a opinar</Components.Paragraph>
+                           <Components.GhostButton onClick={() => toggle(false)}>Registrarse</Components.GhostButton> 
+                     </Components.RightOverlayPanel>
+ 
+                 </Components.Overlay>
+             </Components.OverlayContainer>
+
+         </Components.Container>
+     )
 }
  
 export default InicioSesion;
