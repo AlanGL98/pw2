fc6a77330697c64e1ee19ab912ab407ccc1dcf8c
1es1ie0 <leslie-rosas@hotmail.com>
2022-11-19 20:02:06 -0600

    intento de registro de usuario

--- app_yopino/src/componentes/vistas/InicioSesion.js
+++ app_yopino/src/componentes/vistas/InicioSesion.js
@@ -24,23 +24,39 @@ class InicioSesion extends Component{
             image: ""
         });
 
+        // Este event es un parámetro que se puede recibir en todas las funciones que sean desencadenadas por un evento de React.
+        const handleOnSubmitRegister=(event)=>{
+            event.preventDefault();
+            console.log("jala");
+          }
 
+          // Esto se agrega porque al utilizar un valor como user.name, este no puede cambiar. Hay que utilizar el onChange para poder cambiar el valor de mi variable name
+          const handleOnChangeInput = (event) => { 
+            const {name, value} = event.target; // Utilizo Destructuring, obtengo el name del input y el valor 
+            setUser({
+                ...user, // Esto es Destructuring, pone todos los atributos que estén contenidos en User, así sobreescribe la información con base en el name de mi input. 
+                [name]: value
+            }) // No tengo idea de por qué funciona si no hace referencia a los otros valores como email, password y photo
+        } 
+          
         return(
             <Components.Container>
                 <Components.SignUpContainer signinIn={this.state.signIn}>
-                    <Components.Form>
-                        <Components.Title>Crear cuenta</Components.Title>
-                        <Components.Input type='text' placeholder='Nombre' />
-                        <Components.Input type='text' placeholder='Apellido paterno' />
-                        <Components.Input type='text' placeholder='Apellido materno' />
-                        <Components.Input type='text' placeholder='Nombre de usuario' />
-                        <Components.Input type='email' placeholder='Email' />
-                        <Components.Input type='date' placeholder='Fecha de nacimiento' />
-                        <Components.Input type='password' placeholder='Contraseña' />
-                        <Components.Input type='password' placeholder='Confirmar contraseña' />
-                        <Link to={"/"}><Components.Button>Registrarse</Components.Button></Link>
-                       
-                    </Components.Form>
+                    <form onSubmit={handleOnSubmitRegister}>
+                        <Components.Form>
+                            <Components.Title>Crear cuenta</Components.Title>
+                            <Components.Input type='text' name="name" value={user.name} onChange={handleOnChangeInput} placeholder='Nombre' />
+                            <Components.Input type='text' name="last_name1"value={user.last_name1} onChange={handleOnChangeInput}  placeholder='Apellido paterno' />
+                            <Components.Input type='text' name="last_name2" value={user.last_name2} onChange={handleOnChangeInput} placeholder='Apellido materno' />
+                            <Components.Input type='text' name="username" value={user.username} onChange={handleOnChangeInput} placeholder='Nombre de usuario' />
+                            <Components.Input type='email' name="email" value={user.email} onChange={handleOnChangeInput} placeholder='Email' />
+                            <Components.Input type='password' name="password" value={user.password} onChange={handleOnChangeInput} placeholder='Contraseña' />
+                            <Components.Input type='password' placeholder='Confirmar contraseña' />
+                            <Components.Input type='date' name="birthdate" value={user.birthdate} onChange={handleOnChangeInput} placeholder='Fecha de nacimiento' />
+                            <Link to={"/"}><Components.Button>Registrarse</Components.Button></Link>
+                        
+                        </Components.Form>
+                    </form>
                 </Components.SignUpContainer>
    
                 <Components.SignInContainer signinIn={this.state.signIn}>
