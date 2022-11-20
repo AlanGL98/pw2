080c6059dd67b58f48306d8a243f0d947dc0cd0f
1es1ie0 <leslie-rosas@hotmail.com>
2022-11-19 20:33:44 -0600

    Update InicioSesion.js
    intento de registro de usuario 2

--- app_yopino/src/componentes/vistas/InicioSesion.js
+++ app_yopino/src/componentes/vistas/InicioSesion.js
@@ -1,49 +1,56 @@
-import React, {Component,useState} from "react";
+import React ,{useState}from 'react';
 import * as Components from '../../elementos/logsign';
 import { Link } from 'react-router-dom';
+// import {Helmet} from 'react-helmet';
+// import HeaderDiv from '../HeaderDiv';
+// const InicioSesion = () => {
+//     return (  
+//         <>
+//             <Helmet>
+//                 <title>Inicio de sesion</title>
+//             </Helmet>
+            
+//             <HeaderDiv titulo="Iniciar sesion"/>
 
-class InicioSesion extends Component{
-    state = {
-        signIn: true,
-    }
+//         </>
 
-    toggle(_var){
-        this.setState({signIn: _var});
-    }
+        
+//     );
+// }
 
-    render(){
-        const [user, setUser] = this.useState({  // Inicializo estas variables de estado con valores vacíos 
-            name: "",
-            last_name1: "",
-            last_name2: "",
-            username: "",
-            email: "",
-            password: "",
-            birthdate: "",
-            id_rol:"63685f0eebc852362f53c40f",
-            image: ""
-        });
+function InicioSesion() {
+    const [signIn, toggle] = React.useState(true);
+    const [user, setUser] = useState({  // Inicializo estas variables de estado con valores vacíos 
+        name: "",
+        last_name1: "",
+        last_name2: "",
+        username: "",
+        email: "",
+        password: "",
+        birthdate: "",
+        id_rol:"63685f0eebc852362f53c40f",
+        image: ""
+    });
 
-        // Este event es un parámetro que se puede recibir en todas las funciones que sean desencadenadas por un evento de React.
-        const handleOnSubmitRegister=(event)=>{
-            event.preventDefault();
-            console.log("jala");
-          }
+    // Este event es un parámetro que se puede recibir en todas las funciones que sean desencadenadas por un evento de React.
+    const handleOnSubmitRegister=(event)=>{
+        event.preventDefault();
+        console.log("jala");
+      }
 
-          // Esto se agrega porque al utilizar un valor como user.name, este no puede cambiar. Hay que utilizar el onChange para poder cambiar el valor de mi variable name
-          const handleOnChangeInput = (event) => { 
-            const {name, value} = event.target; // Utilizo Destructuring, obtengo el name del input y el valor 
-            setUser({
-                ...user, // Esto es Destructuring, pone todos los atributos que estén contenidos en User, así sobreescribe la información con base en el name de mi input. 
-                [name]: value
-            }) // No tengo idea de por qué funciona si no hace referencia a los otros valores como email, password y photo
-        } 
-          
-        return(
-            <Components.Container>
-                <Components.SignUpContainer signinIn={this.state.signIn}>
-                    <form onSubmit={handleOnSubmitRegister}>
-                        <Components.Form>
+      // Esto se agrega porque al utilizar un valor como user.name, este no puede cambiar. Hay que utilizar el onChange para poder cambiar el valor de mi variable name
+      const handleOnChangeInput = (event) => { 
+        const {name, value} = event.target; // Utilizo Destructuring, obtengo el name del input y el valor 
+        setUser({
+            ...user, // Esto es Destructuring, pone todos los atributos que estén contenidos en User, así sobreescribe la información con base en el name de mi input. 
+            [name]: value
+        }) // No tengo idea de por qué funciona si no hace referencia a los otros valores como email, password y photo
+    } 
+      
+return(
+    <Components.Container>
+        <Components.SignUpContainer signinIn={signIn}>
+                        <Components.Form onSubmit={handleOnSubmitRegister}>
                             <Components.Title>Crear cuenta</Components.Title>
                             <Components.Input type='text' name="name" value={user.name} onChange={handleOnChangeInput} placeholder='Nombre' />
                             <Components.Input type='text' name="last_name1"value={user.last_name1} onChange={handleOnChangeInput}  placeholder='Apellido paterno' />
@@ -53,43 +60,41 @@ class InicioSesion extends Component{
                             <Components.Input type='password' name="password" value={user.password} onChange={handleOnChangeInput} placeholder='Contraseña' />
                             <Components.Input type='password' placeholder='Confirmar contraseña' />
                             <Components.Input type='date' name="birthdate" value={user.birthdate} onChange={handleOnChangeInput} placeholder='Fecha de nacimiento' />
-                            <Link to={"/"}><Components.Button>Registrarse</Components.Button></Link>
+                            <Link to={"/"}><Components.Button type='submit'>Registrarse</Components.Button></Link>
                         
                         </Components.Form>
-                    </form>
-                </Components.SignUpContainer>
-   
-                <Components.SignInContainer signinIn={this.state.signIn}>
-                     <Components.Form>
-                         <Components.Title>Iniciar Sesion</Components.Title>
-                         <Components.Input type='text' placeholder='Usuario' />
-                         <Components.Input type='password' placeholder='Password' />
-                       
-                         <Link to={"/"}><Components.Button>Iniciar Sesion</Components.Button></Link>
-                     </Components.Form>
-                </Components.SignInContainer>
-   
-                <Components.OverlayContainer signinIn={this.state.signIn}>
-                    <Components.Overlay signinIn={this.state.signIn}>
-   
-                    <Components.LeftOverlayPanel signinIn={this.state.signIn}>
-                        <Components.Title>Bienvenido de vuelta</Components.Title>
-                        <Components.Paragraph> Inicia sesión para seguir dando tus opiniones</Components.Paragraph>
-                        <Components.GhostButton onClick={() => this.toggle(true)}> Iniciar Sesión </Components.GhostButton>
-                        </Components.LeftOverlayPanel>
-   
-                        <Components.RightOverlayPanel signinIn={this.state.signIn}>
-                          <Components.Title>Bienvenido</Components.Title>
-                          <Components.Paragraph>Llena tu informacion y comienza a opinar</Components.Paragraph>
-                              <Components.GhostButton onClick={() => this.toggle(false)}>Registrarse</Components.GhostButton> 
-                        </Components.RightOverlayPanel>
-    
-                    </Components.Overlay>
-                </Components.OverlayContainer>
-   
-            </Components.Container>
-        )
-    }
+        </Components.SignUpContainer>
+
+        <Components.SignInContainer signinIn={signIn}>
+             <Components.Form>
+                 <Components.Title>Iniciar Sesion</Components.Title>
+                 <Components.Input type='text' placeholder='Usuario' />
+                 <Components.Input type='password' placeholder='Password' />
+               
+                 <Link to={"/"}><Components.Button>Iniciar Sesion</Components.Button></Link>
+             </Components.Form>
+        </Components.SignInContainer>
+
+        <Components.OverlayContainer signinIn={signIn}>
+            <Components.Overlay signinIn={signIn}>
+
+            <Components.LeftOverlayPanel signinIn={signIn}>
+                <Components.Title>Bienvenido de vuelta</Components.Title>
+                <Components.Paragraph> Inicia sesión para seguir dando tus opiniones</Components.Paragraph>
+                <Components.GhostButton onClick={() => toggle(true)}> Iniciar Sesión </Components.GhostButton>
+                </Components.LeftOverlayPanel>
+
+                <Components.RightOverlayPanel signinIn={signIn}>
+                  <Components.Title>Bienvenido</Components.Title>
+                  <Components.Paragraph>Llena tu informacion y comienza a opinar</Components.Paragraph>
+                      <Components.GhostButton onClick={() => toggle(false)}>Registrarse</Components.GhostButton> 
+                </Components.RightOverlayPanel>
+
+            </Components.Overlay>
+        </Components.OverlayContainer>
+
+    </Components.Container>
+)
 }
 
 export default InicioSesion;
